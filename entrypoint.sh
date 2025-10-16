5989384348956673570701ca36c7035202204147
Ernesto Junior <ernesto.sjunior@hotmail.com>
2025-10-15 22:38:46 -0300

    fix: bind Gunicorn to all interfaces in production mode

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,5 @@ if [ "$ENV" = "development" ]; then
   exec python3 -m flask run --host=127.0.0.1 --port=4000
 else
   echo "🚀 Starting in production mode..."
-  exec gunicorn -w 4 -b 127.0.0.1:4000 wsgi:app
+  exec gunicorn -w 4 -b 0.0.0.0:4000 wsgi:app
 fi
