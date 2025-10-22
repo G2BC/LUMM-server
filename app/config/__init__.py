1e347b87fea7e95d2aaf6cd7936f7c44f83463df
Ernesto Junior <ernesto.sjunior@hotmail.com>
2025-10-22 12:15:46 -0300

    fix: update OPENAPI_URL_PREFIX to an empty string and add API_SPEC_OPTIONS for server configuration

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -5,7 +5,7 @@ class Config:
     API_TITLE = "LUMM"
     API_VERSION = "v1"
     OPENAPI_VERSION = "3.0.3"
-    OPENAPI_URL_PREFIX = "/api"
+    OPENAPI_URL_PREFIX = ""
     OPENAPI_SWAGGER_UI_PATH = "/docs"
     OPENAPI_SWAGGER_UI_URL = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
     SQLALCHEMY_DATABASE_URI = os.getenv(
@@ -14,3 +14,4 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY", "changeme")
     API_KEY = os.getenv("API_KEY", "")
+    API_SPEC_OPTIONS = {"servers": [{"url": "/api"}]}
