4b6cc6a54cabc70c2a9235cce1d1c1833800dcb3
Ernesto Junior <ernesto.sjunior@hotmail.com>
2025-10-22 11:57:46 -0300

    fix: set OPENAPI_URL_PREFIX to '/api' for correct API routing

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -5,7 +5,7 @@ class Config:
     API_TITLE = "LUMM"
     API_VERSION = "v1"
     OPENAPI_VERSION = "3.0.3"
-    OPENAPI_URL_PREFIX = ""
+    OPENAPI_URL_PREFIX = "/api"
     OPENAPI_SWAGGER_UI_PATH = "/docs"
     OPENAPI_SWAGGER_UI_URL = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
     SQLALCHEMY_DATABASE_URI = os.getenv(
